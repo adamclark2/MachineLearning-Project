708372dee499956de3fca7ccaa928bc3fcb7ec47
Adam Clark <adam.clark2@maine.edu>
2018-12-01 12:07:50 -0500

    Fixed Data Continuity Issues by modifying the ETLScript and re-generating data.

--- data/Data Processing.sql
+++ data/Data Processing.sql
@@ -1,3 +1,6 @@
+--  Nov is 11
+--  APR is 4
+
 -- Insert Data Into Table
 -- I did this for each csv file
 INSERT into UMSDataNew SELECT *, 2018 as 'Year', 11 as 'Month' FROM UMSData;
@@ -9,3 +12,8 @@ SELECT * FROM UMSData;
 
 -- All the CSV Files...
 SELECT distinct Year, Month From UMSDataNew;
+
+
+-- Delete All
+DELETE FROM UMSData WHERE 1=1;
+DELETE FROM UMSDataNew WHERE 1=1;
--- data/Gender View.sql
+++ data/Gender View.sql
@@ -1,3 +1,17 @@
+-- Names by gender contains
+-- The names by gender table contains
+-- Occurences of the names by gender
+--
+-- Turns out there are male and female 'Stephen'(s)
+SELECT name,sex,max(count) FROM NamesByGender
+GROUP BY name;
+
+
+-- Remove views
+drop view tmp1;
+drop view genderTable;
+
+
 -- Code to get genders
 CREATE VIEW tmp1 as
 SELECT
@@ -11,27 +25,27 @@ as First_Name_And_Middle,
   *
 From UMSDataNew;
 
-
-
-
 CREATE VIEW genderTable as
 SELECT
     substr(
         First_Name_And_Middle,0, charindex(' ', First_Name_And_Middle)
     )
     as First_Name,
-  *
+  *, MAX(count)
 From tmp1
-LEFT JOIN NamesByGender ON NamesByGender.name == First_Name ;
+LEFT JOIN NamesByGender ON NamesByGender.name == First_Name
+GROUP BY CMP,Formatted_Name,Title,Salbase,JobSt,"Bargaining Unit","Faculty Appt",FTE,Year,Month;
 
+SELECT * FROM genderTable where name='Stephen';
+
+SELECT * FROM UMSDataNew;
 SELECT * FROM genderTable;
 
-
--- Remove views
-drop view tmp1;
-drop view genderTable;
 
 -- Get a count
 SELECT sex, count(*)
 from genderTable
 group by sex;
+
+
+SELECT * from genderTable where Salbase >= 500000;
